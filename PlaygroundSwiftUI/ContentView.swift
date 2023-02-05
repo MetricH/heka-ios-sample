575497c564b702045a52727758ae70089b599c67
Pulkit Goyal <7895pulkit@gmail.com>
2023-02-06 01:49:07 +0530

    check in ios sample app

---
+++ PlaygroundSwiftUI/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  PlaygroundSwiftUI
+//
+//  Created by Gaurav Tiwari on 04/02/23.
+//
+
+import SwiftUI
+import heka
+
+struct ContentView: View {
+    var body: some View {
+      NavigationView {
+        HekaUIView(
+          uuid: "<user-uuid-goes-here>",
+          apiKey: "<your-api-key-goes-here>"
+        )
+        .padding()
+        .navigationTitle("Heka Preview")
+      }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
